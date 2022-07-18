7e952ec291d0677c21692c4c7b3d8059b1e36877
hecronk <damiraminov1@yandex.ru>
2022-07-18 18:58:37 +0500

    init

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    TELEGRAM_BOT_TOKEN = os.environ['BOT_TOKEN']
+    
